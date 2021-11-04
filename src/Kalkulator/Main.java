f3aa81e4a8c92c18387f83aa407abde783f945ec
Ana Obradovic <anaobradovic2001@gmail.com>
2021-11-04 14:17:45 +0100

    initial read loop

--- src/Kalkulator/Main.java
+++ src/Kalkulator/Main.java
@@ -1,10 +1,18 @@
 package Kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+		String s;
+		do {
+		 s= scanner.nextLine();
+			}while(!s.equals("kraj"));
 
-	}
-
+}
 }
